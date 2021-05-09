dc75cc07477e46450c7d3e0068040128487eb9e3
kumatsum <sumit.a.kumar@tieto.com>
2021-05-09 13:23:00 +0530

    Added a comment

--- src/com/learning/creational/factory/CarFactoryMain.java
+++ src/com/learning/creational/factory/CarFactoryMain.java
@@ -1,7 +1,7 @@
 package com.learning.creational.factory;
 
 public class CarFactoryMain {
-
+// plain Factory Main Method	
 	public static void main(String[] args) {
 		CarFactory carFactory = new CarFactory();
 		ICar sedanCar = carFactory.getCar("Sedan");
